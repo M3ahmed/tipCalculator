1a4460505de005d81741632892bad23a1b595188
Mohamed Ahmed <m3ahmed@ucsd.edu>
2020-01-23 18:21:05 -0800

    first commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Mohamed S Ahmed on 1/23/20.
+//  Copyright © 2020 Red Iris Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
